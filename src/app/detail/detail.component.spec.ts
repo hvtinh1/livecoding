0af0ca09f1bf9226522d6a5e77fe2e012309630b
hvtinh1 <hoangvutinh1@gmail.com>
2023-10-09 11:13:33 +0700

    Add testcase for detail on ngOnInit

--- src/app/detail/detail.component.spec.ts
+++ src/app/detail/detail.component.spec.ts
@@ -14,7 +14,12 @@ describe('DetailComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [DetailComponent],
-      imports: [RouterTestingModule, HttpClientModule, MaterialModule,FormsModule],
+      imports: [
+        RouterTestingModule,
+        HttpClientModule,
+        MaterialModule,
+        FormsModule,
+      ],
     });
     fixture = TestBed.createComponent(DetailComponent);
     component = fixture.componentInstance;
@@ -24,19 +29,42 @@ describe('DetailComponent', () => {
     expect(component).toBeTruthy();
   });
   it('should render detail page title', () => {
-    const h3ElementContext =
-      fixture.nativeElement.querySelector('.mat-mdc-card-title').textContent;
+    const h3ElementContext = fixture.nativeElement.querySelector(
+      '.mat-mdc-card-title'
+    ).textContent;
     expect(h3ElementContext).toContain('Detail page');
-
   });
 
-  it('should render correct data', () => {
+  it('should render correct detail data', () => {
     component.detail = mockData[0];
     fixture.detectChanges();
     const h3ElementContext =
       fixture.nativeElement.querySelector('h3').textContent;
     expect(h3ElementContext).toContain(mockData[0].firstName);
     expect(h3ElementContext).toContain(mockData[0].lastName);
+  });
 
+  it('should render correct data after init', () => {
+    component.prescriberData = mockData;
+    fixture.detectChanges();
+    let testData: string[] = [];
+    const h3ElementContext = fixture.nativeElement.querySelectorAll('tr');
+    let count = 0;
+
+    [...h3ElementContext].map((trElement: any, index: number) => {
+      if (index === 0) return;
+      testData = testData.concat(
+        [...trElement.childNodes].reduce((x, y) => {
+          return x.concat([y.innerText]);
+        }, [])
+      );
+    });
+    for (let i of Object.values(mockData)) {
+      for (let j of Object.values(i)) {
+        if (j === null) expect('').toBe(testData[count].toString());
+        else expect(j.toString()).toBe(testData[count].toString());
+        count++;
+      }
+    }
   });
 });
